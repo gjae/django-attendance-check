2e5fa5cc83a2d3abbeed07ada377ea90f3aadd4d
Gio <gjavilae@gmail.com>
2023-12-08 13:11:57 -0400

    Fix

--- src/src/settings/middlewares.py
+++ src/src/settings/middlewares.py
@@ -1,3 +1,4 @@
+import logging
 from django.http import HttpResponse
 from django.utils import translation
 from django.conf import settings
@@ -18,11 +19,14 @@ class ClientMiddleware:
         en ese caso se debe consutar la lista de IP de clientes para verificar si 
         continua con la petición o se retorna un error
         """
+        log = logging.getLogger(__name__)
 
+        log.info(f"Ip request: {request.META.get('REMOTE_ADDR', '0.0.0.0')} / {request.META.get('HTTP_X_REAL_IP', '0.0.0.0')}")
+        log.info(request.META)
         if "admin" in request.get_full_path() or "error" in request.get_full_path():
             return self.get_response(request)
         
-        if ClientConfig.objects.is_enabled(request.META.get("REMOTE_ADDR", "0.0.0.0")):
+        if ClientConfig.objects.is_enabled(request.META.get("HTTP_X_REAL_IP", "0.0.0.0")):
             return self.get_response(request)
         
 
