14b4ff3427bb565caf6550da8d1a0a79b7beb04b
Gio <gjavilae@gmail.com>
2024-01-24 09:45:24 -0400

    Fix

--- src/src/clocking/signals.py
+++ src/src/clocking/signals.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
@@ -18,5 +19,29 @@ def on_create_observation_save_checking(sender, instance: DailyCalendarObservati
     if check_counter == 1 and DailyCalendarObservation.OBSERVATION_TYPE_CHOICES.checkin:
         return None
     
+    now = datetime.now(instance.calendar_day.date_day.year, instance.calendar_day.date_day.month, instance.calendar_day.date_day.day, 8,0,0,0)
+    end = now + timedelta(hours=8)
 
-    return DailyChecks.objects.create()
+    if check_counter == 0 and instance.check_type == 3:
+        DailyChecks.objects.create(
+            employee=instance.employer,
+            checking_type=0,
+            time=now.time(),
+            checking_time=now
+        )
+        DailyChecks.objects.create(
+            employee=instance.employer,
+            checking_type=1,
+            time=end.time(),
+            checking_time=now
+        )
+
+    elif check_counter == 1 and instance.check_type == 2:
+        f = checks_by_user.first()
+        end = f.checking_time + timedelta(hours=8)
+        DailyChecks.objects.create(
+            employee=instance.employer,
+            checking_type=1,
+            time=end.time(),
+            checking_time=now
+        )
