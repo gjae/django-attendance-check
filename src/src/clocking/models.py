5665d29f1578a81add2e6d2959e52ea0a8f7158a
Giovanny J. Avila E <gjavilae@gmail.com>
2024-05-23 14:42:57 -0400

    Fix: orden de reportes

--- src/src/clocking/models.py
+++ src/src/clocking/models.py
@@ -43,7 +43,7 @@ class DailyChecks(TimeStampedModel):
 
 
     class Meta:
-        ordering = ["-checking_time", "employee_id", "-id"]
+        ordering = ["-daily__date_day", "-checking_time", "employee_id", "-id"]
         verbose_name = "Chequeo del día"
         verbose_name_plural = "Chequeos diarios"
 
