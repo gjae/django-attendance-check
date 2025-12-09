9f153bee0e00aaf087789b4e820331a43aef20bc
Giovanny J. Avila E <gjavilae@gmail.com>
2025-12-09 10:53:06 -0400

    FIX

--- src/src/reports/utils.py
+++ src/src/reports/utils.py
@@ -4,7 +4,8 @@ from typing import Iterable
 def has_observation(record: dict, observations: Iterable) -> bool:
     for observation in observations:
         if observation.employer is not None:
-            if record["employer"]["id"] == observation.employer.id and observation.calendar_day_id == record["daily_id"]:
+            employer_id = record["employer"]["id"] if isinstance(record["employer"], dict) else record["employer"].id
+            if employer_id == observation.employer.id and observation.calendar_day_id == record["daily_id"]:
                 return True
         else:
             if record["person"]["id"] == observation.person.id and observation.calendar_day_id == record["daily_id"]:
